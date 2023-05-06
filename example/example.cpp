a77d53c04b1dd311597f43b529639e6d06a3fd27
Kyrylo Sovailo <k.sovailo@gmail.com>
2023-05-06 20:10:26 +0200

    GTest/unittest written

--- example/example.cpp
+++ example/example.cpp
@@ -1,7 +1,16 @@
 #include "../include/devtemplate/devtemplate.h"
+#include <gtest/gtest.h>
 
-int main()
+TEST(Example, Example)
 {
-    devtemplate::Devtemplate devtemplate;
-    return devtemplate.devtemplate();
+    devtemplate::Devtemplate *d;
+    EXPECT_NO_THROW(d = new devtemplate::Devtemplate());
+    EXPECT_EQ(d->devtemplate(), 0);
+    EXPECT_NO_THROW(delete d);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
--- example/example.py
+++ example/example.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
-import os, sys
+import os, sys, unittest
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/build")
 import devtemplate
 
+class Example(unittest.TestCase):
+    def test_devtemplate(self):
+        d = devtemplate.Devtemplate()
+        self.assertEqual(d.devtemplate(), 0)
+
 if __name__ == "__main__":
-    d = devtemplate.Devtemplate()
-    r = d.devtemplate()
-    sys.exit(r)
+    unittest.main()
