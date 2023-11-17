27a35ca0e5f5d5de4a8de5f4e98d5148dc1b496e
Kyrylo Sovailo <k.sovailo@gmail.com>
2023-11-17 02:51:55 +0100

    Very minor fixes

--- config/test.cmake
+++ config/test.cmake
@@ -11,7 +11,7 @@ endif()
 add_executable(${DEV_CMAKE_NAME}_test)
 list(APPEND DEV_EXPORT_TARGETS ${DEV_CMAKE_NAME}_test)
 list(APPEND DEV_PACKAGE_TARGETS ${DEV_CMAKE_NAME}_test)
-set_target_properties(${DEV_CMAKE_NAME}_executable_gui PROPERTIES OUTPUT_NAME "${DEV_FILE_NAME}_test")
+set_target_properties(${DEV_CMAKE_NAME}_test PROPERTIES OUTPUT_NAME "${DEV_FILE_NAME}_test")
 
 # Link dependencies
 target_link_libraries(${DEV_CMAKE_NAME}_test PRIVATE ${DEV_CMAKE_NAME})
